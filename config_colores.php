43bde9a60f1f6640ea76b1547d15ad177c424c68
Alejandro <alejandrombc@gmail.com>
2017-05-03 19:45:29 -0400

    Nuevo colores y noticia

--- config_colores.php
+++ config_colores.php
@@ -1,15 +1,29 @@
 <?php
 	// DEFINICION DE VARIABLES GLOBALES DE COLORES
 	
-	define("COLOR_LETRA_PARRAFO",  "#FFF");
+	define("COLOR_LETRA_PARRAFO",  "#000"); //USADO
 
-	define("COLOR_LETRA_LISTA",  "#FFF");
+	define("COLOR_LETRA_LISTA",  "#474a4c"); //USADO
 
-	define("COLOR_LETRA_LINKS",  "#00F"); //USADO
+	define("COLOR_FONDO_LISTA",  "#e3e3e3"); //USADO
 
-	define("COLOR_LETRA_BOTON",  "#FFF");
+	define("COLOR_LETRA_LINKS",  "#FFF"); //USADO
 
-	define("COLOR_LETRA_HEADER",  "#FFF");
+	define("COLOR_LINKS_HOVER",  "#77797c"); //USADO
+
+	define("COLOR_LETRA_LINKS_HOVER",  "#dee0e2"); //USADO
+
+	define("COLOR_LETRA_LISTA_LINKS",  "#000"); //USADO
+
+	define("COLOR_LETRA_BOTON",  "#000"); //USADO
+
+	define("COLOR_FONDO_BOTON",  "#FFF"); //USADO
+
+	define("COLOR_LETRA_BOTON_HOVER",  "#dee0e2"); //USADO
+ 
+	define("COLOR_FONDO_BOTON_HOVER",  "#77797c"); //USADO
+
+	define("COLOR_LETRA_HEADER",  "#000"); //USADO
 
 	define("COLOR_LETRA_LABEL",  "#FFF");
 
@@ -17,21 +31,17 @@
 
 	define("COLOR_FONDO_INPUT",  "#FFF");
 
-	define("COLOR_DIVS",  "#FFF");
+	define("COLOR_FONDO_MENU_DERECHO",  "#d4d9db"); //USADO
 
-	define("COLOR_BUTTON",  "#FFF");
+	define("COLOR_FONDO_MENU_IZQUIERDO",  "#d4d9db"); //USADO
 
-	define("COLOR_CONTAINER",  "#FFF");
+	define("COLOR_CONTAINER",  "#bbbfc1"); //USADO
 
 
 
-	define("COLOR_NAVBAR",  "#FFF"); //USADO
+	define("COLOR_NAVBAR",  "#4e4f51"); //USADO
 
-	define("COLOR_HOVER",  "#F00"); //USADO
+	define("COLOR_ACTIVE_LINK",  "#616366"); //USADO
 
-	define("COLOR_LETRA_HOVER",  "#0F0"); //USADO
-
-	define("COLOR_ACTIVE_LINK",  "#FFF"); //USADO
-
-	define("COLOR_BACKGROUND",  "#FFF"); 
+	define("COLOR_BACKGROUND",  "#FFF"); //USADO
 ?>
--- noticia.php
+++ noticia.php
@@ -1,8 +1,8 @@
 <?php
-	define("NOTICIA",  false);
+	define("NOTICIA",  true);
 
 	include('config_general.php'); //INCLUSION DEL ARCHIVO DE CONFIGURACION GENERAL
-
+	
 	include('templates/index.html'); //INCLUSION DEL HTML PRINCIPAL
 
 ?>
